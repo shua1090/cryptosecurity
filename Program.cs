f51db2daaea832939568599648eec1a9af950c32
Shynn <shynn.lawrence@gmail.com>
2020-12-11 21:29:50 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using cryptosecurity;
+
+namespace cryptosecurity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var z = new encrypt("HOHO");
+            Console.WriteLine(z.z);
+        }
+    }
+}
---
+++ encrypt/encrypt.cs
@@ -0,0 +1,8 @@
+namespace cryptosecurity{
+    public class encrypt{
+        public string z;
+        public encrypt(string b){
+            this.z = b;
+        }
+    }
+}
