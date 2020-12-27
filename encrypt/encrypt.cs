07602ea1aa3c344f19f10f881d88df6ea91c8a0a
Shynn Lawrence <61219106+shua1090@users.noreply.github.com>
2020-12-26 19:29:58 -0800

    Enabled BigInteger everywhere: Humungous Numbers

--- encrypt/encrypt.cs
+++ encrypt/encrypt.cs
@@ -10,12 +10,10 @@ namespace cryptosecurity {
         BigInteger phi;
 
         public void setupNPhi() {
-            BigInteger f = largePrimeNumber() * largePrimeNumber() * largePrimeNumber();
-            BigInteger k = largePrimeNumber() * largePrimeNumber() * largePrimeNumber();
+            BigInteger f = largePrimeNumber();
+            BigInteger k = largePrimeNumber();
             this.n = f * k;
             this.phi = (f - 1) * (k - 1);
-            Console.WriteLine(this.n);
-            Console.WriteLine(this.phi);
         }
 
         public encrypt(string text){
@@ -23,29 +21,22 @@ namespace cryptosecurity {
             setupNPhi();
         }
 
-        public static int NextInt32() {
-            var rand = new Random();
-            int firstBits = rand.Next(0, 1 << 4) << 28;
-            int lastBits = rand.Next(0, 1 << 28);
-            return firstBits | lastBits;
-        }
-
-        public decimal randomDecimal(){
-            var rand = new Random();
-            byte scale = (byte) rand.Next(29);
-            bool sign = rand.Next(2) == 1;
-            return new decimal(NextInt32(), NextInt32(), NextInt32(), sign, scale);
+        public BigInteger randomBigInt(){
+            Random rand = new Random();
+            byte[] data = new byte[100000];
+            rand.NextBytes(data);
+            return new BigInteger(data);
         }
 
         public BigInteger largePrimeNumber() {
-            decimal num1 = 0;
+            BigInteger num1 = 0;
             while (((num1 < long.MaxValue) || (num1 < long.MinValue)) || (!IsPrime(num1))) {
-                num1 = this.randomDecimal();
+                num1 = this.randomBigInt();
             }
-            return (BigInteger) num1;
+            return num1;
         }
 
-        public static bool IsPrime(decimal number){
+        public static bool IsPrime(BigInteger number){
             if (number < 2) return false;
             if (number % 2 == 0) return (number == 2);
             int root = (int) Math.Sqrt((double)number);
